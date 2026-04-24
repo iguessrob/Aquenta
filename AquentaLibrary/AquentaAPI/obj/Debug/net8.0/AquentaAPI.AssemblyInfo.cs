7be2a2f0e6c99e84d9e90ee3c00dca0bd50e59c5
Angeline Ricajoy Manalo <manalogeline2@gmail.com>
2026-04-25 03:39:56 +0800

    update manual

--- AquentaLibrary/AquentaAPI/obj/Debug/net8.0/AquentaAPI.AssemblyInfo.cs
+++ AquentaLibrary/AquentaAPI/obj/Debug/net8.0/AquentaAPI.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AquentaAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4f1f502fd85073dd8e4a750e2206a2ac459303ca")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+04f97b602852b143394caddf572367d4c7d0bf1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("AquentaAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AquentaAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
